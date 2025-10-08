b3b0a34c7f09ff78b74aac23eb3b6c71dd881ed3
shinubo28always <shinubo28always@gmail.com>
2025-10-08 18:46:24 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    API_ID = int(os.getenv("API_ID", 0))
+    API_HASH = os.getenv("API_HASH", "")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+    OWNER_ID = int(os.getenv("OWNER_ID", 0))
+    TARGET_CHAT_ID =  # 👈 ekhane tumi group/channel ID boshabe (e.g. -1001234567890)
