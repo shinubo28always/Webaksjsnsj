e971eb91548611c22c40d85082b59ba28bddaf6e
shinubo28always <shinubo28always@gmail.com>
2025-10-16 08:40:35 +0600

    Refactor environment variable loading in config.py

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    API_ID = int(os.getenv("API_ID", 0))
-    API_HASH = os.getenv("API_HASH", "")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-    OWNER_ID = int(os.getenv("OWNER_ID", 0))
-    TARGET_CHAT_ID =  # 👈 ekhane tumi group/channel ID boshabe (e.g. -1001234567890)
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", 0))
+    API_HASH = os.environ.get("API_HASH", "")
+    OWNER_ID = int(os.environ.get("OWNER_ID", 0))
