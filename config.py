baa2c8880c294f4177089d4dd6c7abb91f4e37c0
shinubo28always <shinubo28always@gmail.com>
2025-10-20 08:11:55 +0600

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", 0))
-    API_HASH = os.environ.get("API_HASH", "")
-    OWNER_ID = int(os.environ.get("OWNER_ID", 0))
