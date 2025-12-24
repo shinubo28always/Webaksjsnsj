a064a7107a5d13dd026a711875260234c92e985b
shinubo28always <shinubo28always@gmail.com>
2025-12-25 00:02:11 +0600

    Update db.py

--- db.py
+++ db.py
@@ -1,10 +1,14 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from config import MONGO_URL
+from config import MONGO_URL, ADMIN_IDS
 
-mongo = AsyncIOMotorClient(MONGO_URL)
-database = mongo["DogeshBhai_S4S_Bot"]
+# Connection
+client = AsyncIOMotorClient(MONGO_URL)
+database = client["DogeshBhai_S4S_Bot"]
+
+# Collections
 users = database.users
 orders = database.orders
+admins = database.admins
 
 async def get_user(uid: int):
     user = await users.find_one({"user_id": uid})
@@ -17,3 +21,11 @@ async def get_user(uid: int):
         }
         await users.insert_one(user)
     return user
+
+async def is_admin(uid):
+    # Pehle check karo Super Admin (Config se)
+    if uid in ADMIN_IDS:
+        return True
+    # Phir check karo Database wale Admins
+    admin = await admins.find_one({"user_id": uid})
+    return True if admin else False
